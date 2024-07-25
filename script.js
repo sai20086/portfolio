8196fe7e4f0752b461be3ba708d342a2c5e02ac1
sai20086 <65651997+sai20086@users.noreply.github.com>
2024-07-25 22:56:09 +0530

    script.js

--- script.js
+++ script.js
@@ -1,74 +1,37 @@
-const audio = new Audio('assets/sentmessage.mp3');
-const contactString = `
-<div class='social'>
-    <a target='_blank' href='tel:+918600765857'>
-        <div class='socialItem' id='call'>
-            <img class='socialItemI' src='images/phone.svg'/>
-            <label class='number'>8600765857</label>
-        </div>
-    </a>
-    <a href='mailto:patilvinu777@gmail.com'>
-        <div class='socialItem'>
-            <img class='socialItemI' src='images/gmail.svg' alt=''>
-        </div>
-    </a>
-    <a target='_blank' href='https://github.com/Vinayak-09'>
-        <div class='socialItem'>
-            <img class='socialItemI' src='images/github.svg' alt=''>
-        </div>
-    </a>
-    <a target='_blank' href='https://wa.me/918600765857'>
-        <div class='socialItem'>
-            <img class='socialItemI' src='images/whatsapp.svg' alt=''>
-        </div>
-    </a>
-    <a target='_blank' href='https://t.me/vinayak_09'>
-        <div class='socialItem'>
-            <img class='socialItemI' src='images/telegram.svg' alt=''>
-        </div>
-    </a>
-    <a target='_blank' href='https://instagram.com/vinayak_patil_09'>
-        <div class='socialItem'>
-            <img class='socialItemI' src='images/instagram.svg' alt=''>
-        </div>
-    </a>
-    <a href='https://www.linkedin.com/in/vinayak-patil-793bb5206/' target='_blank' rel='noopener noreferrer'>
-        <div class='socialItem'>
-            <img class='socialItemI' src='images/linkedin.svg' alt=''>
-        </div>
-    </a>
-</div>`;
-
-const resumeString = `
-<img src='images/resumeThumbnail.png' class='resumeThumbnail'>
-<div class='downloadSpace'>
-    <div class='pdfname'>
-        <img src='images/pdf.png'>
-        <label>Vinayak Resume.pdf</label>
-    </div>
-    <a href='assets/Vinayak Patil&#39;s Resume.pdf' download='Vinayak_Patil_Resume.pdf'>
-        <img class='download' src='images/downloadIcon.svg'>
-    </a>
-</div>`;
-
-const addressString = `
-<div class='mapview'>
-    <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238.63833262443757!2d74.19014864534314!3d16.865338763272877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1a7dcf40f5dd7%3A0xd7b69fe1fcfa9877!2zMTbCsDUxJzU1LjQiTiA3NMKwMTEnMjUuMyJF!5e0!3m2!1sen!2sin!4v1645079906766!5m2!1sen!2sin' class='map'></iframe>
-</div>
-<label class='add'>
-    <address>B2 'Asara'<br>Kodoli<br>Kolhapur, Maharashtra, INDIA 416114</address>
-</label>`;
-
+var audio = new Audio('assets/sentmessage.mp3');
+var contactString = "<div class='social'> <a target='_blank' href='tel:+918600765857'> <div class='socialItem' id='call'><img class='socialItemI' src='images/phone.svg'/><label class='number'>8600765857</label></div> </a> <a href='mailto:patilvinu777@gmail.com'> <div class='socialItem'><img class='socialItemI' src='images/gmail.svg' alt=''></div> </a> <a target='_blank' href='https://github.com/Vinayak-09'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a> <a target='_blank' href='https://wa.me/918600765857'> <div class='socialItem'><img class='socialItemI' src='images/whatsapp.svg' alt=''></div> </a> <a target='_blank' href='https://t.me/vinayak_09'> <div class='socialItem'><img class='socialItemI' src='images/telegram.svg' alt=''></div> </a> <a target='_blank' href='https://instagram.com/vinayak_patil_09'> <div class='socialItem'><img class='socialItemI' src='images/instagram.svg' alt=''> </div> </a> <a href='https://www.linkedin.com/in/vinayak-patil-793bb5206/' target='_blank' rel='noopener noreferrer'> <div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div> </a> </div>";
+var resumeString = "<img src='images/resumeThumbnail.png' class='resumeThumbnail'><div class='downloadSpace'><div class='pdfname'><img src='images/pdf.png'><label>Vinayak Resume.pdf</label></div><a href='assets/Vinayak Patil&#39;s Resume.pdf' download='Vinayak_Patil_Resume.pdf'><img class='download' src='images/downloadIcon.svg'></a></div>";
+var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d238.63833262443757!2d74.19014864534314!3d16.865338763272877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc1a7dcf40f5dd7%3A0xd7b69fe1fcfa9877!2zMTbCsDUxJzU1LjQiTiA3NMKwMTEnMjUuMyJF!5e0!3m2!1sen!2sin!4v1645079906766!5m2!1sen!2sin' class='map'></iframe></div><label class='add'><address>B2 'Asara'<br>Kodoli<br>Kolhapur, Maharashtra, INDIA 416114</address>";
 function startFunction() {
     setLastSeen();
     waitAndResponce("intro");
 }
 
 function setLastSeen() {
-    const date = new Date();
-    const lastSeen = document.getElementById("lastseen");
-    lastSeen.innerText = `last seen today at ${date.getHours()}:${date.getMinutes()}`;
+    var date = new Date();
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "last seen today at " + date.getHours() + ":" + date.getMinutes()
 }
+
+
+function closeFullDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+function openFullScreenDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
 
 function isEnter(event) {
     if (event.keyCode == 13) {
@@ -77,88 +40,133 @@ function isEnter(event) {
 }
 
 function sendMsg() {
-    const input = document.getElementById("inputMSG");
-    const ti = input.value;
-    if (input.value === "") return;
-
-    const date = new Date();
-    const myLI = document.createElement("li");
-    const myDiv = document.createElement("div");
-    const greendiv = document.createElement("div");
-    const dateLabel = document.createElement("label");
-
-    dateLabel.innerText = `${date.getHours()}:${date.getMinutes()}`;
+    var input = document.getElementById("inputMSG");
+    var ti = input.value;
+    if (input.value == "") {
+        return;
+    }
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
     myDiv.setAttribute("class", "sent");
     greendiv.setAttribute("class", "green");
     dateLabel.setAttribute("class", "dateLabel");
     greendiv.innerText = input.value;
-
     myDiv.appendChild(greendiv);
     myLI.appendChild(myDiv);
     greendiv.appendChild(dateLabel);
     document.getElementById("listUL").appendChild(myLI);
-    document.getElementById("chatting").scrollTop = document.getElementById("chatting").scrollHeight;
-
-    setTimeout(() => waitAndResponce(ti), 1500);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    setTimeout(function () { waitAndResponce(ti) }, 1500);
     input.value = "";
     playSound();
 }
 
 function waitAndResponce(inputText) {
-    const lastSeen = document.getElementById("lastseen");
+    var lastSeen = document.getElementById("lastseen");
     lastSeen.innerText = "typing...";
     switch (inputText.toLowerCase().trim()) {
         case "intro":
             setTimeout(() => {
-                sendTextMessage(`Hello there 👋🏻 <br>I'm Vinayak Patil, a computer engineering student currently exploring and working on various projects.`);
-            }, 1000);
+                sendTextMessage("Hello there 👋🏻,<br><br>My name is <span class='bold'><a class='alink'>Vinayak Ramchandra Patil</a>.</span><br><br>I am a Computer Engineering student at <span class='bold'>Ch. Shivaji University 👨🏻‍💻📚</span><br><br>I am eager to hear about potential career opportunities, so I would be pleased to chat about job openings in the engineering sphere.<br><br>Send <span class='bold'>'help'</span> to know more about me.<br>");
+            }, 2000);
+            break;
+        case "help":
+            sendTextMessage("<span class='sk'>Send Keyword to get what you want to know about me...<br>e.g<br><span class='bold'>'skills'</span> - to know my skills<br><span class='bold'>'resume'</span> - to get my resume<br><span class='bold'>'education'</span> - to get my education details<br><span class='bold'>'address'</span> - to get my address<br><span class='bold'>'contact'</span> - to get ways to connect with me<br><span class='bold'>'projects'</span> - to get details of my projects<br><span class='bold'>'clear'</span> - to clear conversation<br><span class='bold'>'about'</span> - to know about this site</span>");
             break;
         case "resume":
-            setTimeout(() => {
-                sendTextMessage(resumeString);
-            }, 1500);
+            sendTextMessage(resumeString);
             break;
+        case "skills":
+            sendTextMessage("<span class='sk'>I am currently pursuing B.Tech degree in Computer Science Engineering.<br><br>I can comfortably write code in following languages :<br><span class='bold'>Java<br>C++<br>C<br>PHP<br>Kotlin<br>Dart<br>Python<br>CSS<br>HTML</span><br><br>I've experiance with following frameworks :<span class='bold'><br>Android<br>Flutter<br>ReactJs<br>GTK</span><br><br>I use <span class='bold'>Arch Linux</span> as daily driver on my HP Pavilion 15-ec0xxx<br>OS:Arch Linux<br>DE:Gnome(More often) Kde(often)<br>Favourite IDE:VSCode</span>");
+            break;
+
+        case "education":
+            sendTextMessage("I am currently pusuing B.Tech degree in Computer Science Engineering from TKIET Kolhapur<br>Passing Year : 2023<br><br>I have completed my Diploma from Government Polytechnic Karad<br>Passing Year:2020<br>Result:86.06%<br><br>I have completed my Secondary school from local school known as SWV<br>Passing Year:2016");
+            break;
+
         case "address":
-            setTimeout(() => {
-                sendTextMessage(addressString);
-            }, 1500);
+            sendTextMessage(addressString);
+            break;
+        case "clear":
+            clearChat();
+            break;
+        case "about":
+            sendTextMessage("🛠️💻 This portfolio website is built using HTML, CSS and JavaScript from SCRATCH!<br><br>👨🏻‍💻 Designed and Developed by <a class='alink' target='_blank' href='https:\/\/instagram.com/vinayak_patil_09/'><span class='bold'>Vinayak Patil</a> with ❤️</span>");
             break;
         case "contact":
-            setTimeout(() => {
-                sendTextMessage(contactString);
-            }, 1500);
+            sendTextMessage(contactString);
             break;
         case "projects":
-            setTimeout(() => {
-                sendTextMessage("I'm currently working on various interesting projects. You can check out my GitHub for more details: <a href='https://github.com/Vinayak-09' target='_blank'>GitHub Profile</a>");
-            }, 1500);
+            sendTextMessage("You want to check my projects? Then just jump into my Github Account.<br><br><div class='social'><a target='_blank' href='https://github.com/Vinayak-09'> <div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div> </a></div>");
+            break;
+        case "new":
+            sendTextMessage(addressString);
             break;
         default:
             setTimeout(() => {
-                sendTextMessage("Sorry, I didn't understand that. You can ask about my resume, address, contact, or projects.");
-            }, 1500);
+                sendTextMessage("Hey I couldn't catch you...😢<br>Send 'help' to know more about usage.");
+            }, 2000);
+            break;
     }
+
+
+
 }
 
-function sendTextMessage(response) {
-    const date = new Date();
-    const receivedLI = document.createElement("li");
-    const receivedDiv = document.createElement("div");
-    const greydiv = document.createElement("div");
-    const dateLabel = document.createElement("label");
+function clearChat() {
+    document.getElementById("listUL").innerHTML = "";
+    waitAndResponce('intro');
+}
 
-    dateLabel.innerText = `${date.getHours()}:${date.getMinutes()}`;
-    receivedDiv.setAttribute("class", "received");
-    greydiv.setAttribute("class", "grey");
+
+
+function sendTextMessage(textToSend) {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.setAttribute("id", "sentlabel");
+    dateLabel.id = "sentlabel";
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    greendiv.innerHTML = textToSend;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+
+function sendResponse() {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
     dateLabel.setAttribute("class", "dateLabel");
-    greydiv.innerHTML = response;
-
-    receivedDiv.appendChild(greydiv);
-    receivedLI.appendChild(receivedDiv);
-    greydiv.appendChild(dateLabel);
-    document.getElementById("listUL").appendChild(receivedLI);
-    document.getElementById("chatting").scrollTop = document.getElementById("chatting").scrollHeight;
-    setLastSeen();
+    greendiv.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ";
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
 }
 
 function playSound() {
